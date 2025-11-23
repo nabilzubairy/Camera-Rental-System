6184d8ed9e58b5450e19b72adcc9776a5b34e868
nabilzubairy <142969317+nabilzubairy@users.noreply.github.com>
2025-11-23 20:22:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+import Home from './component/Home';
+import About from './component/About';
+import Contact from './component/Contact'
+import Login from './Pages/Login';
+import Signup from './Pages/Signup';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './component/Layout';
+import { AuthProvider } from './context/AuthContext';
+import Cart from './component/Cart';
+import AddCamera from './component/AddCamera';
+import Cameras from './component/Cameras';
+import Booking from './component/Booking';
+import Payment from './component/Payment';
+import Confirm from './component/Confirm';
+
+function App() {
+  return (
+    <Router>
+      <Layout>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/add-camera" element={<AddCamera />} />
+          <Route path="/cameras" element={<Cameras />} />
+          <Route path="/booking" element={<Booking />} />
+          <Route path='/payment' element={<Payment />} />
+          <Route path='/confirm' element={<Confirm />} />
+
+
+        </Routes>
+
+      </Layout>
+    </Router>
+  );
+}
+
+export default App;
+
+
---
+++ src/component/Layout.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+
+
+function Layout({children, isLoggedIn, setIsLoggedIn}) {
+
+    
+
+    return (
+        <div className="d-flex flex-column min-vh-100">
+            <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+            <main className="flex-fill">{children}</main>
+            <Footer />
+        </div>
+    )
+}
+
+export default Layout;
